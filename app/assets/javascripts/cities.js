6745a0286d4f656b817e00d3373c470d3a3ae22a
ronyrtan <ron.yr.tan@hotmail.com>
2016-11-23 18:32:00 +1100

    added disqus

--- app/assets/javascripts/cities.js
+++ app/assets/javascripts/cities.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
   var hashString = window.location.hash;
   console.log(hashString);
+
+  $('.catAll').addClass('active');
+
+  $('.categorySelect a').click(function() {
+    $('.categorySelect a').removeClass('active');
+    $(this).addClass('active');
+  });
 });
