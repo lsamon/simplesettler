6a71b50ecb828fcd8ff4fb499d9c572e1b8382fc
ronyrtan <ron.yr.tan@hotmail.com>
2016-11-25 10:31:59 +1100

    removed dead code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,25 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # before_action :fetch_user
-
   private
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_in) do |user_params|
         user_params.permit(:username, :email)
       end
     end
-    # def fetch_user
-    #   if session[:user_id].present?
-    #     @current_user = User.find_by :id => session[:user_id]
-    #   end
-    #   session[:user_id] = nil unless @current_user.present?
-    # end
-    #
-    # def check_for_user
-    #   redirect_to root_path unless @current_user.present?
-    # end
-    #
+
     def check_for_admin
       redirect_to root_path unless (current_user.present? && current_user.admin?)
     end
