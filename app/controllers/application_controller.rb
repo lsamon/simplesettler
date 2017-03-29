575b91933655f0b7cd4360658cf5a4e6704d1393
lsamon <louis.amon@amazutech.net>
2017-03-29 22:44:44 +1100

    Added login page for admin and added css for Dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :current_city, :check_for_admin
+  layout :layout_by_resource
 
   def current_city
     session[:city_id] = params[:city_id] ? params[:city_id] : params[:id]
@@ -18,6 +19,10 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_in) do |user_params|
         user_params.permit(:username, :email)
       end
+    end
+
+    def layout_by_resource
+      "user" if devise_controller?
     end
 
 end
