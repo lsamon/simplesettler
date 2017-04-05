9731613ea2d6fc518b9494f8e8c3a91426635c8e
lsamon <louis.amon@amazutech.net>
2017-04-05 10:25:07 +1000

    deleted featuredimage migration

--- db/migrate/20170224034549_remove_columns_from_articles.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColumnsFromArticles < ActiveRecord::Migration
-  def change
-    remove_column :articles, :featured_image_file_name, :string
-    remove_column :articles, :featured_image_content_type, :string
-    remove_column :articles, :featured_image_updated_at, :datetime
-    remove_column :articles, :featured_image_file_size, :integer
-  end
-end
