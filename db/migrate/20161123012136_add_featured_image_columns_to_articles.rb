94053fdcc4bd0fc96069045cb1e7fda3dc36c6b8
lsamon <louis.amon@amazutech.net>
2017-04-05 10:21:05 +1000

    deleted featuredimage migration

--- db/migrate/20161123012136_add_featured_image_columns_to_articles.rb
+++
@@ -1,9 +0,0 @@
-class AddFeaturedImageColumnsToArticles < ActiveRecord::Migration
- def up
-    add_attachment :articles, :featured_image
-  end
-
-  def down
-    remove_attachment :articles, :featured_image
-  end
-end
