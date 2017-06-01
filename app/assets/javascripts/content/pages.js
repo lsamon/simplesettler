d008c5b9d8b049148081e2508352294bba1c5219
Niroj Sapkota <xenial.nickel@gmail.com>
2017-06-01 11:38:50 +1000

    working on redesigning the navbar

--- app/assets/javascripts/content/pages.js
+++ app/assets/javascripts/content/pages.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
   $(document).scroll(function(){
-    if ($(document).scrollTop() > 200) {
+    /*if ($(document).scrollTop() > 200) {
       $('nav').css({
         background: 'rgba(0,0,0,0.8)',
         transition: '.3s'
       });
     } else {
       $('nav').css('background', 'rgba(0,0,0,0)');
-    }
+    }*/
 
 
   });
