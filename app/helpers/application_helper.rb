1682111cd42a209688aac7dbddc1b310ef238534
Niroj Sapkota <xenial.nickel@gmail.com>
2017-06-04 22:21:36 +1000

    fixed issue with new user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,6 +18,7 @@ module ApplicationHelper
 
 
   def country_name(country_code)
+    return "Not set" if country_code.nil?
     country = ISO3166::Country[country_code]
     country.translations[I18n.locale.to_s] || country.name
   end
--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -2,10 +2,12 @@ class UserDetail < ActiveRecord::Base
   belongs_to :User
 
   def full_name
+    return nil if self.f_name.nil?
     self.f_name+" "+self.l_name
   end
 
   def get_visa_name
+    return "Not Set" if self.visa_status.nil?
     visa = VisaType.find(self.visa_status)
     !visa.nil? ? visa[:name] : "Not set"
   end
