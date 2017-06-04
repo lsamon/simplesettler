0dba7f6af303d3b2a75ebcd6c1dbd9c1516684b7
lsamon <louis.amon@amazutech.net>
2017-06-04 14:12:35 +1000

    fixed issues with package and user login and signup

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,5 +1,10 @@
 class Package < ActiveRecord::Base
-
   enum status: { draft: 0, published: 1, unpublished: 2 }
   has_many :payments
+  before_save :increment_id, if: :new_record?
+
+  private
+  def increment_id
+    self.id = Package.last.id + 1
+  end
 end
