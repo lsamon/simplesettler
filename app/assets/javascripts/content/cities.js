946db0c2787b1e60b699430419c92ab3415fc3fb
lsamon <louis.amon@amazutech.net>
2017-06-08 04:00:38 +1000

    added active category javascript

--- app/assets/javascripts/content/cities.js
+++ app/assets/javascripts/content/cities.js
@@ -7,10 +7,13 @@ $(document).ready(function(){
   var category = getUrlParams(window.location.href)[1];
   var city = getUrlParams(window.location.href)[0];
 
+  console.log($('.' + category));
+  $('.' + category + ' a').addClass('active');
+
   $.ajax({
     type: 'GET',
     url: '/cities/'+city+'/'+'categories/'+category
-  })
+  });
 
   $('.categorySelect a').click(function() {
     $('.categorySelect a').removeClass('active');
