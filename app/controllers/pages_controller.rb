ec3e6a0f633d73ddf6daf499565c53dc4bcbedf6
lsamon <louis.amon@amazutech.net>
2017-06-26 21:11:49 +1000

    applied layout to the relevant pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,23 +1,24 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:help]
-  layout 'application'
+  layout 'pages'
   def index
     @cities = City.all
     @articles = Article.all
     @banner = Banner.first
+    render layout: 'application'
   end
 
   def help
     @visa_types = VisaType.all
-    render layout: 'pages'
   end
 
   def about
     @article = Article.about_us
-    render layout: 'pages'
   end
 
   def privacy
   end
 
+  def tos
+  end
 end
