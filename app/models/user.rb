b5d34d5020c6d47ae079bada75d78f2a241a2179
lsamon <louis.amon@amazutech.net>
2017-06-27 13:08:58 +1000

    Skip confirmation if omniauth account

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,8 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.username = auth.info.name   # assuming the user model has a name
+      user.email = auth.info.email
+      user.skip_confirmation!
     end
   end
 
