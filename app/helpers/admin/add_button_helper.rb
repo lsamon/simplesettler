241474d9992a2a5a64d1acc4294fc02fe163ad24
lsamon <louis.amon@amazutech.net>
2017-12-30 16:22:38 +1100

    improved dashaboard methods

--- app/helpers/admin/add_button_helper.rb
+++ app/helpers/admin/add_button_helper.rb
@@ -1,13 +1,10 @@
 module Admin::AddButtonHelper
-  def add_button_title(url)
-    get_title_from_url(url)&.titleize
+  def add_button_title
+    controller_name&.titleize
   end
 
-  def add_link_location(url)
-    add_button_title(url) == 'Admin' ? "/admin/articles/new" : "/admin/#{get_title_from_url(url)}/new"
+  def add_link_location
+    "/admin/#{controller_name}/new"
   end
 
-  def get_title_from_url(url)
-    url.split("/").last.split("?").first
-  end
 end
---
+++ app/helpers/admin/navigation_helper.rb
@@ -0,0 +1,11 @@
+module Admin::NavigationHelper
+  def side_menu(controller, icon)
+    title = controller.titleize
+    content_tag :li, title,  class: "nav-item #{controller_name == controller ? 'active': 'test'}" do
+      link_to("/admin/#{controller}") do
+        concat content_tag :i, '', class: "fa #{icon}", 'aria-hidden': 'true'
+        concat content_tag :span, controller.titleize, class: 'hidden-xs hidden-sm'
+      end
+    end
+  end
+end
